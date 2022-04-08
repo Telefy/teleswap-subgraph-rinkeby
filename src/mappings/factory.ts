ab77d3ff14626326ba0df225269361f3253861eb
lokisanhitleson <lokesh603@gmail.com>
2022-04-08 18:49:42 +0530

    initial commit

---
+++ src/mappings/factory.ts
@@ -0,0 +1,112 @@
+import { log } from "@graphprotocol/graph-ts"
+import { PairCreated } from "../../generated/Factory/Factory"
+import { Bundle, Pair, TeleswapFactory, Token } from "../../generated/schema"
+import { fetchTokenDecimals, fetchTokenName, fetchTokenSymbol, fetchTokenTotalSupply, ZERO_BD, ZERO_BI } from "./helpers"
+import { Pair as PairTemplate } from '../../generated/templates'
+
+export function handlePairCreated(event: PairCreated): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let factory = TeleswapFactory.load(event.transaction.from.toHex())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!factory) {
+    factory = new TeleswapFactory(event.transaction.from.toHex())
+    factory.pairCount = 0
+    factory.totalVolumeETH = ZERO_BD
+    factory.totalLiquidityETH = ZERO_BD
+    factory.totalVolumeUSD = ZERO_BD
+    factory.untrackedVolumeUSD = ZERO_BD
+    factory.totalLiquidityUSD = ZERO_BD
+    factory.txCount = ZERO_BI
+
+    // create new bundle
+    let bundle = new Bundle('1')
+    bundle.ethPrice = ZERO_BD
+    bundle.save()
+  }
+  factory.pairCount = factory.pairCount + 1
+
+  // Entities can be written to the store with `.save()`
+  factory.save()
+
+  // create the tokens
+  let token0 = Token.load(event.params.token0.toHexString())
+  let token1 = Token.load(event.params.token1.toHexString())
+
+
+  // fetch info if null
+  if (!token0) {
+    token0 = new Token(event.params.token0.toHexString())
+    token0.symbol = fetchTokenSymbol(event.params.token0)
+    token0.name = fetchTokenName(event.params.token0)
+    token0.totalSupply = fetchTokenTotalSupply(event.params.token0)
+    let decimals = fetchTokenDecimals(event.params.token0)
+
+    // bail if we couldn't figure out the decimals
+    if (!decimals) {
+      log.debug('mybug the decimal on token 0 was null', [])
+      return
+    }
+
+    token0.decimals = decimals
+    token0.derivedETH = ZERO_BD
+    token0.tradeVolume = ZERO_BD
+    token0.tradeVolumeUSD = ZERO_BD
+    token0.untrackedVolumeUSD = ZERO_BD
+    token0.totalLiquidity = ZERO_BD
+    token0.txCount = ZERO_BI
+  }
+
+  // fetch info if null
+  if (!token1) {
+    token1 = new Token(event.params.token1.toHexString())
+    token1.symbol = fetchTokenSymbol(event.params.token1)
+    token1.name = fetchTokenName(event.params.token1)
+    token1.totalSupply = fetchTokenTotalSupply(event.params.token1)
+    let decimals = fetchTokenDecimals(event.params.token1)
+
+    // bail if we couldn't figure out the decimals
+    if (!decimals) {
+      return
+    }
+    token1.decimals = decimals
+    token1.derivedETH = ZERO_BD
+    token1.tradeVolume = ZERO_BD
+    token1.tradeVolumeUSD = ZERO_BD
+    token1.untrackedVolumeUSD = ZERO_BD
+    token1.totalLiquidity = ZERO_BD
+    // token1.allPairs = []
+    token1.txCount = ZERO_BI
+  }
+
+  let pair = new Pair(event.params.pair.toHexString()) as Pair
+  pair.token0 = token0.id
+  pair.token1 = token1.id
+  pair.liquidityProviderCount = ZERO_BI
+  pair.createdAtTimestamp = event.block.timestamp
+  pair.createdAtBlockNumber = event.block.number
+  pair.txCount = ZERO_BI
+  pair.reserve0 = ZERO_BD
+  pair.reserve1 = ZERO_BD
+  pair.trackedReserveETH = ZERO_BD
+  pair.reserveETH = ZERO_BD
+  pair.reserveUSD = ZERO_BD
+  pair.totalSupply = ZERO_BD
+  pair.volumeToken0 = ZERO_BD
+  pair.volumeToken1 = ZERO_BD
+  pair.volumeUSD = ZERO_BD
+  pair.untrackedVolumeUSD = ZERO_BD
+  pair.token0Price = ZERO_BD
+  pair.token1Price = ZERO_BD
+
+  // create the tracked contract based on the template
+  PairTemplate.create(event.params.pair)
+
+  // save updated values
+  token0.save()
+  token1.save()
+  pair.save()
+  factory.save()
+}
